38ddfbb9fcced161a40bbdfa31dbca2a941c0ab0
BhupalReddySama262318 <bhupalreddysama18@gmail.com>
2025-01-27 16:44:14 +0530

    add changes

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -24,7 +24,8 @@ from django.contrib.auth.urls import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')), 
-    path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', lambda request: redirect('tweet/', permanent=True)), 
 ] + static(settings.MEDIA_URL, document_root=settings.
 MEDIA_ROOT)
  
