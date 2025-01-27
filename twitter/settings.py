6e48fc595295d8dbecd20e34561f57a81b9d16d2
BhupalReddySama262318 <bhupalreddysama18@gmail.com>
2025-01-27 19:38:33 +0530

    change

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p@_e^9rlgfcuqot8rmnxv48u0gcm3iaj765r%4tsacc=zjc&o=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['//tweethub-ki95.onrender.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['tweethub-ki95.onrender.com','127.0.0.1', 'localhost']
 
 
 # Application definition
